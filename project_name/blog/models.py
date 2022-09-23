52be9b8813c031e1b79c4f3f0afe6e42977e01e1
Ali Özden <99548828+ali38ozden@users.noreply.github.com>
2022-09-23 15:24:22 +0300

    ders 16 django modls

--- project_name/blog/models.py
+++ project_name/blog/models.py
@@ -1,3 +1,16 @@
+from email.mime import image
+from pyexpat import model
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title= models.CharField(max_length=200)
+    image= models.CharField(max_length=50)
+    description= models.TextField()
+    is_acitve= models.BooleanField()
+    is_home= models.BooleanField()
+
+class Catogory(models.Model):
+    name= models.CharField(max_length=150)
+
+
